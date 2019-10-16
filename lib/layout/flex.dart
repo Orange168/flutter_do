25a4a91394502a9c128e8911db7c4e4dfd362d80
leavesC <1990724437@qq.com>
2019-10-16 10:54:30 +0800

    flex

--- lib/layout/flex.dart
+++ lib/layout/flex.dart
@@ -18,6 +18,67 @@ class FlexPage extends BaseDemoPage {
         mainAxisAlignment: MainAxisAlignment.start,
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[]);
+        children: <Widget>[
+          Flex(
+            direction: Axis.horizontal,
+            children: <Widget>[
+              Expanded(
+                flex: 2,
+                child: Container(
+                  height: 60.0,
+                  color: Colors.lightBlue[500],
+                ),
+              ),
+              Expanded(
+                flex: 4,
+                child: Container(
+                  height: 60.0,
+                  color: Colors.teal[500],
+                ),
+              ),
+            ],
+          ),
+          Flex(
+            direction: Axis.horizontal,
+            children: <Widget>[
+              Expanded(
+                flex: 2,
+                child: Container(
+                  height: 60.0,
+                  color: Colors.deepOrange[500],
+                ),
+              ),
+              Expanded(
+                flex: 5,
+                child: Container(
+                  height: 60.0,
+                  color: Colors.orange[500],
+                ),
+              ),
+            ],
+          ),
+          Flex(
+            direction: Axis.horizontal,
+            children: <Widget>[
+              Expanded(
+                flex: 2,
+                child: Container(
+                  height: 60.0,
+                  color: Colors.pinkAccent[400],
+                ),
+              ),
+              Spacer(
+                flex: 1,
+              ),
+              Expanded(
+                flex: 6,
+                child: Container(
+                  height: 60.0,
+                  color: Colors.grey[500],
+                ),
+              ),
+            ],
+          ),
+        ]);
   }
 }
