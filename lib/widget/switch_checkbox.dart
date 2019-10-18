b7e5de14aa41b93b6904202e9f6fe635e9f47b5f
leavesC <1990724437@qq.com>
2019-10-18 17:28:55 +0800

    Switch Checkbox

--- lib/widget/switch_checkbox.dart
+++ lib/widget/switch_checkbox.dart
@@ -8,7 +8,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_do/common/page.dart';
 
 class SwitchCheckboxPage extends BaseDemoPage {
-  SwitchCheckboxPage() : super('Flutter Switch Checkbox Demo');
+  SwitchCheckboxPage() : super('Switch Checkbox', includeScrollView: false);
 
   @override
   Widget generateChildren(BuildContext context) {
@@ -33,41 +33,45 @@ class _SwitchCheckboxState extends State<_SwitchCheckboxWidget> {
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: Column(
-      children: <Widget>[
-        Row(
-          children: <Widget>[
-            Switch(
-              value: switchSelected,
-              activeColor: Colors.deepOrange,
-              activeTrackColor: Colors.amber,
-              inactiveThumbColor: Colors.cyanAccent,
-              inactiveTrackColor: Colors.deepPurple,
-              onChanged: (value) {
-                setState(() {
-                  switchSelected = value;
-                });
-              },
-            ),
-            Text(switchSelected ? "选中" : "未选中"),
-          ],
-        ),
-        Row(
-          children: <Widget>[
-            Checkbox(
-              value: checkboxSelected,
-              activeColor: Colors.deepOrange,
-              checkColor: Colors.amber,
-              onChanged: (value) {
-                setState(() {
-                  checkboxSelected = value;
-                });
-              },
-            ),
-            Text(checkboxSelected ? "选中" : "未选中"),
-          ],
-        ),
-      ],
-    ));
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Switch(
+                value: switchSelected,
+                activeColor: Colors.blue[300],
+                activeTrackColor: Colors.blue[500],
+                inactiveThumbColor: Colors.grey[400],
+                inactiveTrackColor: Colors.grey[600],
+                onChanged: (value) {
+                  setState(() {
+                    switchSelected = value;
+                  });
+                },
+              ),
+              Text(switchSelected ? "选中" : "未选中"),
+            ],
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Checkbox(
+                value: checkboxSelected,
+                activeColor: Colors.red[200],
+                checkColor: Colors.blue[500],
+                onChanged: (value) {
+                  setState(() {
+                    checkboxSelected = value;
+                  });
+                },
+              ),
+              Text(checkboxSelected ? "选中" : "未选中"),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
