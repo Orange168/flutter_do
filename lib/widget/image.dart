3fd81587778b1257656c6a3c91499538de96d205
leavesC <1990724437@qq.com>
2019-10-18 15:47:19 +0800

    image

--- lib/widget/image.dart
+++ lib/widget/image.dart
@@ -8,49 +8,86 @@ import 'package:flutter/material.dart';
 import 'package:flutter_do/common/page.dart';
 
 class ImagePage extends BaseDemoPage {
-  ImagePage() : super('Flutter Image Demo');
+  ImagePage() : super('Image');
 
   @override
   Widget generateChildren(BuildContext context) {
     return Column(
-        children: <Widget>[
-      _ImageWithTipWidget(
-          widget: Image.network(
-            "https://cn.bing.com/th?id=OIP.ULMiN7DES66fDdI22MVC2AHaJo&pid=Api&rs=1",
-            width: 100,
-            height: 100,
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.all(20),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Image.network(
+                  "https://cn.bing.com/th?id=OIP.ULMiN7DES66fDdI22MVC2AHaJo&pid=Api&rs=1",
+                  width: 100,
+                  height: 100,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "网络图片",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 12,
+                  ),
+                )
+              ],
+            ),
           ),
-          tip: "网络图片"),
-      getImage(color: null, colorBlendMode: null, tip: "本地资源图片"),
-    ]..addAll(getImageList()));
+        ),
+        Padding(
+          padding: EdgeInsets.all(20),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Image.asset(
+                  "assets/images/avatar.jpg",
+                  width: 100,
+                  height: 100,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "本地资源图片",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 12,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ]..addAll(getImageList()),
+    );
   }
 }
 
 List<Widget> getImageList() {
   return BlendMode.values.reversed.map((element) {
-    return getImage(colorBlendMode: element, tip: element.toString());
+    return _ImageWithTipWidget(
+      element,
+      color: Colors.blue,
+    );
   }).toList();
 }
 
-Widget getImage(
-    {Color color = Colors.cyan, BlendMode colorBlendMode, String tip}) {
-  return _ImageWithTipWidget(
-      widget: Image.asset(
-        "assets/images/avatar.jpg",
-        width: 100,
-        height: 100,
-        color: color,
-        colorBlendMode: colorBlendMode,
-      ),
-      tip: tip);
-}
-
 class _ImageWithTipWidget extends StatelessWidget {
-  final String tip;
+  final BlendMode colorBlendMode;
 
-  final Widget widget;
+  final Color color;
 
-  _ImageWithTipWidget({this.widget, this.tip});
+  _ImageWithTipWidget(this.colorBlendMode, {this.color: Colors.lightBlue});
 
   @override
   Widget build(BuildContext context) {
@@ -62,13 +99,22 @@ class _ImageWithTipWidget extends StatelessWidget {
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            widget,
+            Image.asset(
+              "assets/images/avatar.jpg",
+              width: 100,
+              height: 100,
+              color: color,
+              colorBlendMode: colorBlendMode,
+            ),
             SizedBox(
               height: 10,
             ),
             Text(
-              tip,
-              style: TextStyle(color: Colors.black, fontSize: 14),
+              colorBlendMode.toString(),
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 12,
+              ),
             )
           ],
         ),
